b8ea220ba3381b19f4a41826e1cabb1e450f2df0
FarisHollow <mirfaris79@gmail.com>
2023-10-31 12:55:57 +0600

    Footer fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,50 +1,51 @@
-import React from "react"
+import React from "react";
 import {
   AiOutlineGithub,
   AiOutlineLinkedin,
   AiOutlineFacebook,
   AiOutlineMail,
-  AiOutlineWhatsApp,
-} from "react-icons/ai"
+  AiOutlinePhone,
+  AiFillEnvironment,
+} from "react-icons/ai";
 
 const Footer = () => {
+  const address = "Uttara, Dhaka";
+  const phone = "+8801330804697";
+  const email = "mirfaris79@gmail.com";
+
   return (
-    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-      <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
-      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © 2023 Mir Faris<a href="/" className="hover:underline"></a>
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="container mx-auto flex flex-col items-center justify-center md:flex-row md:justify-between">
+        <div className="text-center md:text-left mb-6">
+          <span className="text-2xl font-semibold">© 2023 Mir Faris</span>
+          <a href="/" className="block text-sm text-gray-400 hover:underline">
+            Home
+          </a>
         </div>
-        <div className="flex flex-row items-center justify-center space-x-4 mb-4">
-            <a href="https://github.com/FarisHollow" rel="noreferrer" target="_blank">
-              <div className="icon-container">
-                <AiOutlineGithub className="icon" />
-              </div>
-            </a>
-            <a href="https://www.facebook.com/mir.faris.35" rel="noreferrer" target="_blank">
-              <div className="icon-container">
-                <AiOutlineFacebook className="icon" />
-              </div>
-            </a>
-            <a href="https://www.linkedin.com/in/mirfaris79/" rel="noreferrer" target="_blank">
-              <div className="icon-container">
-                <AiOutlineLinkedin className="icon" />
-              </div>
-            </a>
-            <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCKCGlnjKnglglBsmpVZPvFKhnMKCJthpxctPHlfggSMqhDXPhTwmmvmkflVSnFDXVmGfGhg" rel="noreferrer" target="_blank">
-              <div className="icon-container">
-                <AiOutlineMail className="icon" />
-              </div>
-            </a>
-            <a href="www.example.com" rel="noreferrer" target="_blank">
-              <div className="icon-container">
-                <AiOutlineWhatsApp className="icon" />
-              </div>
-            </a>
+
+        <div className="text-center md:text-left">
+          <div className="mb-4">
+            <span className="text-2xl">
+              <AiOutlineMail />
+            </span>
+            <strong className="ml-2">{email}</strong>
           </div>
+          <div className="mb-4">
+            <span className="text-2xl">
+              <AiOutlinePhone />
+            </span>
+            <strong className="ml-2">{phone}</strong>
+          </div>
+          <div>
+            <span className="text-2xl">
+              <AiFillEnvironment />
+            </span>
+            <strong className="ml-2">{address}</strong>
+          </div>
+        </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
