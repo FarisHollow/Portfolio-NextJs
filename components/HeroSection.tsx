db2a0db038cee4a1d1774d7b0ca22a33645cd54c
Mir Faris <mirfaris79@gmail.com>
2024-04-02 01:30:45 +0600

    Minor change

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -15,8 +15,8 @@ const HeroSection = () => {
           <Image
             src="/faris1.jpg"
             alt=""
-            width={1025}
-            height={1025}
+            width={350}
+            height={350}
             className="rounded-full shadow-2xl border-4 border-blue-500"
           />
         </div>
